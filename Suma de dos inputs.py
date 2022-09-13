0c91015bc332e7933cde50a107084fdb1b25d409
Arduriki <104797917+arduriki@users.noreply.github.com>
2022-09-13 18:53:52 +0200

    Add files via upload

---
+++ Suma de dos inputs.py
@@ -0,0 +1,10 @@
+#identificador=input("dona'm això") // té com a definicio string (str)
+#variables: 1. identificador 2. contingut 3. tipus
+#variable assignació: número=34 // imposes un contingut i el guarda en la variable. També se li diu expressió.
+#si hi han comes, guardes com a caràcter. No com a número.
+#quan tornem a assignar, matxaquem el d'abans.
+
+n1=int(input("Dona'm el primer número: ")) #tal qual serà número enter, sense comes
+n2=int(input("Dona'm el segon número: "))
+suma=n1+n2
+print(suma)
