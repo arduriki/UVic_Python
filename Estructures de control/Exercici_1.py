09cf0be113a49a451e73387e3f1cfe07f3595da8
Arduriki <104797917+arduriki@users.noreply.github.com>
2022-09-19 19:03:24 +0200

    Add files via upload

--- Estructures de control/Exercici_1.py
+++ Estructures de control/Exercici_1.py
@@ -1,36 +1,40 @@
-euros = int(input("Write how many euros you have: "))
+#Operadors:
+# // -> Dividir
+# % -> Residu de la divisio (en aquest cas sería el restant dels diners)
 
-euros500 = euros // 500
-print("You have " + str(euros500) + " five hundred euros note/s")
-euros = euros % 500
+euros = int(input("Write how many euros you have: ")) #variable d'entrada
 
-euros200 = euros // 200
-print("You have " + str(euros200) + " two hundred euros note/s")
+note500 = euros // 500 #divideix la quantitat d'euros per 500
+print("You have " + str(note500) + " five hundred euros note/s")
+euros = euros % 500 #euros que queden
+
+note200 = euros // 200
+print("You have " + str(note200) + " two hundred euros note/s")
 euros = euros % 200
 
-euros100 = euros // 100
-print("You have " + str(euros100) + " one hundred euros note/s")
+note100 = euros // 100
+print("You have " + str(note100) + " one hundred euros note/s")
 euros = euros % 100
 
-euros50 = euros // 50
-print("You have " + str(euros50) + " fifty euros note/s")
+note50 = euros // 50
+print("You have " + str(note50) + " fifty euros note/s")
 euros = euros % 50
 
-euros20 = euros // 20
-print("You have " + str(euros20) + " twenty euros note/s")
+note20 = euros // 20
+print("You have " + str(note20) + " twenty euros note/s")
 euros = euros % 20
 
-euros10 = euros // 10
-print("You have " + str(euros10) + " ten euros note/s")
+note10 = euros // 10
+print("You have " + str(note10) + " ten euros note/s")
 euros = euros % 10
 
-euros5 = euros // 5
-print("You have " + str(euros5) + " five euros note/s")
+note5 = euros // 5
+print("You have " + str(note5) + " five euros note/s")
 euros = euros % 5
 
-euros2 = euros // 2
-print("You have " + str(euros2) + " two euros coin/s")
+coin2 = euros // 2
+print("You have " + str(coin2) + " two euros coin/s")
 euros = euros % 2
 
-euros1 = euros // 1
-print("You have " + str(euros2) + " one euros coin/s")
+coin1 = euros // 1
+print("You have " + str(coin1) + " one euros coin/s")
