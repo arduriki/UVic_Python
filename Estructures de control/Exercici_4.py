1412990885bdb767bec5ff736f23366c408dd7c4
Arduriki <104797917+arduriki@users.noreply.github.com>
2022-09-19 18:07:51 +0200

    Add files via upload

---
+++ Estructures de control/Exercici_4.py
@@ -0,0 +1,13 @@
+#suma +
+#restar -
+#multiplicar *
+#dividir //
+#print = imprimir
+#input = usuari@ introduzca un valor
+#elevado a = **
+
+number_given = float(input("Give me a number in order to square and cube it: "))
+square = int(number_given ** 2)
+cube = int(number_given ** 3)
+
+print("squared number = " + str(square) + "\n cubed number = " + str(cube))
