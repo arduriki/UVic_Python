d4bb979b5c6510cd6789d34cd4d5b160389820d0
Arduriki <104797917+arduriki@users.noreply.github.com>
2022-09-20 17:11:33 +0200

    Add files via upload

--- Exercicis/Exercici_8.py
+++ Exercicis/Exercici_8.py
@@ -1,1 +1,12 @@
 #Fer un programa que donats 3 valors numèrics d’entrada mostri quin és el més gran.
+
+number1 = int(input("Write your first number: "))
+number2 = int(input("Write your second number: "))
+number3 = int(input("Write your third number: "))
+
+if number1 < number2 < number3:
+    print("Biggest number is: " + str(number3))
+elif number1 < number2 > number3:
+    print("Biggest number is " + str(number2))
+else:
+    print("Biggest number is " + str(number1))
