85e6a9eb680ab8b6f7c32e9eacc1ac5ecf7727b2
Arduriki <104797917+arduriki@users.noreply.github.com>
2022-10-03 20:24:49 +0200

    Delete Exercicis directory

--- Exercicis/Exercici_1_condicionals.py
+++
@@ -1,9 +0,0 @@
-euros = int(input("Write how many euros you have: "))
-
-note500 = euros // 500 #divideix la quantitat d'euros per 500
-if note500 != 0: #! diferent / si no es compleix, no facis res
-    print("You have " + str(note500) + " five hundred euros note/s")
-euros = euros % 500 #euros que queden
-
-#else: sino fes això
-#while és el mateix que el if, però se'n torna cap a dalt i entra en bucle quan es va complint.
--- Exercicis/Exercici_7.py
+++
@@ -1,9 +0,0 @@
-#Fer un programa que ens retorni el valor absolut d’un número
-
-num = float(input("Write a number in order to know its absolute value: "))
-
-if num < 0:
-    num = (num * -1)
-    print(str(num) + " is the absolute value")
-else:
-    print(str(num) + " is the absolute value")
